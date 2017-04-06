704c76b219598a4f8ff87ddb67aafcff6d0a15a6
Chris Carlson <ccarlson@panth.com>
2017-04-06 07:17:25 -0400

    apparently the 'ping' class doesn't know how to handle simultaneous async calls

--- NetTools/ContinuousPing.cs
+++ NetTools/ContinuousPing.cs
@@ -27,19 +27,19 @@ namespace chrispyduck.ps.NetTools
         private Ping ping;
         private int interval;
         private int timeout;
-        private bool stop = false;
+        private Task mainLoop;
+        private CancellationTokenSource cts;
 
         protected override Task ProcessRecordAsync()
         {
             this.timeout = this.Timeout ?? 1000;
-            this.interval = this.Interval ?? 1500;
-            if (interval < timeout)
-                interval = timeout;
+            this.interval = this.Interval ?? 1000;
             this.hostRecords = Task.WhenAll(this.Hosts.Select(this.PrepareHost)).Result;
             this.context = SynchronizationContext.Current;
             this.ping = new Ping();
             this.ping.PingCompleted += this.OnPingCompleted;
-            return this.MainLoop();
+            this.cts = new CancellationTokenSource();
+            return this.mainLoop = this.MainLoop();
         }
 
         private async Task<HostRecord> PrepareHost(string host)
@@ -60,16 +60,23 @@ namespace chrispyduck.ps.NetTools
 
         protected override Task StopProcessingAsync()
         {
-            this.stop = true;
-            return Task.FromResult(0);
+            this.cts.Cancel();
+            return this.mainLoop;
         }
 
         private async Task MainLoop()
         {
-            while (!base.Stopping && !this.stop)
+            var hrPairs = this.hostRecords.Select(r => new Tuple<HostRecord, Ping>(r, new Ping())).ToArray();
+
+            // register event handlers
+            foreach (var hrPair in hrPairs)
+                hrPair.Item2.PingCompleted += this.OnPingCompleted;
+
+            while (!base.Stopping && !this.cts.IsCancellationRequested)
             {
-                foreach (var hostRecord in hostRecords)
+                foreach (var hrPair in hrPairs)
                 {
+                    var hostRecord = hrPair.Item1;
                     // skip if in progress
                     if (hostRecord.InProgress)
                         continue;
@@ -82,11 +89,21 @@ namespace chrispyduck.ps.NetTools
 
                     // send new ping
                     hostRecord.LastAttempt = DateTime.Now;
-                    ping.SendAsync(hostRecord.IP, timeout, hostRecord);
+                    hrPair.Item2.SendAsync(hostRecord.IP, timeout, hostRecord);
 
-                    await Task.Delay(interval);
+                    await Task.Delay(interval, this.cts.Token);
                 }
             }
+
+            // unregister event handlers and cleanup
+            foreach (var hrPair in hrPairs)
+            {
+                base.WriteVerbose($"Disposing of ping wrapper for host {hrPair.Item1.Host}");
+                hrPair.Item2.PingCompleted -= this.OnPingCompleted;
+                hrPair.Item2.Dispose();
+            }
+
+            base.WriteVerbose("Exiting main loop");
         }
 
         private void OnPingCompleted(object sender, PingCompletedEventArgs e)
